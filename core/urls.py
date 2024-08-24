b8a2f7db53e7dcc29659bda45252589289b25ca5
faisalPdev <mohdfaisal1459@gmail.com>
2024-08-24 16:52:42 +0530

    Added jquery scripts

--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
+from django.conf import settings
 from django.urls import path,include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
 
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ static/js/script.js
@@ -0,0 +1,55 @@
+
+$(document).ready(function() {
+    var passwordField = $('#id_password');
+    var toggleDiv = $('#passwordToggle');
+
+    // Function to toggle visibility of the checkbox
+    function toggleCheckboxVisibility() {
+        if (passwordField.val()) {
+            toggleDiv.show();
+        } else {
+            toggleDiv.hide();
+        }
+    }
+
+    // Initial check
+    toggleCheckboxVisibility();
+
+    // Check on input change
+    passwordField.on('input', function() {
+        toggleCheckboxVisibility();
+    });
+
+    // Toggle password visibility and label text on checkbox click
+    $('#flexCheckIndeterminate').on('click', function() {
+        var type = passwordField.attr('type') === 'password' ? 'text' : 'password';
+        passwordField.attr('type', type);
+
+        var labelText = (type === 'password') ? 'Show' : 'Hide';
+        $('.form-check-label').text(labelText);
+    });
+
+
+});
+
+$(document).ready(function(){
+
+    $('.submitForm').submit(function (e) { 
+        
+        var password = $('#id_password').val();
+
+
+    
+            // if (password.length < 6) {
+            //         $('.passwordValidation').text('Password must contain 6 characters')
+                    
+            //         e.preventDefault();
+            // }
+
+        
+        
+    });
+})
+
+
+
